c99a2100394bb21b721413f8d3d746ee27bc6ef9
Vadim Dynnik <vadim.dynnik@gmail.com>
2021-08-17 14:10:13 +0300

    Second commit

--- app/src/main/java/ru/krtech/android/test/MainActivity.kt
+++ app/src/main/java/ru/krtech/android/test/MainActivity.kt
@@ -1,18 +1,22 @@
 package ru.krtech.android.test
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var prevButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var prevButton: ImageButton
+    private lateinit var nextButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -27,6 +31,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -36,24 +41,57 @@ class MainActivity : AppCompatActivity() {
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {
+            Log.d(TAG, "TRUE")
             checkAnswer(true)
         }
 
-        falseButton.setOnClickListener {
+        falseButton.setOnClickListener {Log.d(TAG, "FALSE")
+
             checkAnswer(false)
         }
 
         prevButton.setOnClickListener {
-            currentIndex = (currentIndex - 1) % questionBank.size
+            Log.d(TAG, "PREV")
+            if (currentIndex > 0) {
+                currentIndex -= 1
+            } else {
+                currentIndex = questionBank.size - 1
+            }
             updateQuestion()
         }
 
         nextButton.setOnClickListener {
+            Log.d(TAG, "NEXT")
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
 
         updateQuestion()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 
     private fun updateQuestion() {
@@ -74,7 +112,6 @@ class MainActivity : AppCompatActivity() {
             Toast.LENGTH_SHORT
         )
         toast.show()
-
     }
 
 }
